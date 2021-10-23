8cde55befbdadc0b8b1338b002a8d74c9abd55b3
EllieAtWHL <eleanormatthewman@gmail.com>
2021-10-23 10:53:34 +0100

    setup video 9 min 38 secs

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function menuButton() {
-    var x = document.getElementById("myMenu");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
-  }
