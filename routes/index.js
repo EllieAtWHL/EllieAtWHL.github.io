56fa235d8dd23535c516aae59362c6740a40876c
EllieAtWHL <eleanormatthewman@gmail.com>
2021-10-23 13:39:02 +0100

    improved gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello World!')
+    //res.send('Hello')
+    res.render('index')
 })
 
 module.exports = router
